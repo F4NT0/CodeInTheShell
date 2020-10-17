f9cb56d826059c9bd72770ffc23857933681f754
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-10-16 21:31:22 -0300

    new scripts of java and astah

---
+++ scripts/install_java14.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:linuxuprising/java
+sudo apt -y install oracle-java14-installer
+sudo apt -y install oracle-java14-set-default
---
+++ scripts/run_astah_uml.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# INSTALL ASTAH UML
+# install on official website
+
+# Run Astah with your Java version
+astah-uml -nojvchk
+
+# Insert license from Astah
+
+# Uninstall Astah
+#sudo dpkg --remove --force-remove-reinstreq astah-community
+#sudo dpkg --remove --force-remove-reinstreq astah-uml
+#sudo dpkg --remove --force-remove-reinstreq astah-professional
